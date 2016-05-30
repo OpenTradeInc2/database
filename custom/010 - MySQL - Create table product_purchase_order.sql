16595989aff06422a0533364d247bb3760d1d5bc
Rolando Soto <rsoto@zeptoo.com>
2016-05-30 11:41:39 -0600

    add product_purchase_order table

---
+++ custom/010 - MySQL - Create table product_purchase_order.sql
@@ -0,0 +1,5 @@
+CREATE  TABLE `ot_custom_product_purchase_order` (
+  `product_purchase_order_id` INT NOT NULL AUTO_INCREMENT ,
+  `product_id` INT NULL ,
+  `purchase_order_id` INT NULL ,
+  PRIMARY KEY (`product_purchase_order_id`) );
